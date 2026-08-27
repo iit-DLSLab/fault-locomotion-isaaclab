5fb77d2810e49c451a83358c0c6381c209fb0b14
giulioturrisi <giulioturrisi@hotmail.it>
2026-08-27 16:27:22 +0200

    fix mujoco warp for rough terrain

--- source/fault_locomotion_isaaclab/fault_locomotion_isaaclab/tasks/fault_locomotion/go2_env_cfg.py
+++ source/fault_locomotion_isaaclab/fault_locomotion_isaaclab/tasks/fault_locomotion/go2_env_cfg.py
@@ -16,7 +16,13 @@ from isaaclab.utils.configclass import configclass
 from isaaclab.utils.noise import GaussianNoiseCfg, NoiseModelWithAdditiveBiasCfg
 from isaaclab_tasks.utils import PresetCfg
 
-from isaaclab_newton.physics import KaminoPADMMSolverCfg, MJWarpSolverCfg, NewtonCfg
+from isaaclab_newton.physics import (
+    KaminoPADMMSolverCfg,
+    MJWarpSolverCfg,
+    NewtonCfg,
+    NewtonCollisionPipelineCfg,
+    NewtonShapeCfg,
+)
 from isaaclab_ov.physics import OvPhysxCfg
 from isaaclab_physx.physics import PhysxCfg
 from isaaclab.physics import PhysxAutoCfg
@@ -122,16 +128,20 @@ class PhysicsCfg(PresetCfg):
     physx = PhysxAutoCfg(isaacsim_physx=isaacsim_physx, ovphysx=ovphysx)
     newton_mjwarp = NewtonCfg(
         solver_cfg=MJWarpSolverCfg(
-            njmax=150,
-            nconmax=50,
-            cone="elliptic",
+            njmax=1000,
+            nconmax=300,
+            cone="pyramidal",
             impratio=1.0,
+            integrator="implicitfast",
+            use_mujoco_contacts=False,
         ),
-        num_substeps=1,
+        #collision_cfg=NewtonCollisionPipelineCfg(max_triangle_pairs=2_500_000),
+        num_substeps=2,
         debug_mode=False,
+        default_shape_cfg=NewtonShapeCfg(margin=0.0, ke=160000.0, kd=1100.0),
     )
     newton_kamino = NewtonCfg(solver_cfg=KaminoPADMMSolverCfg(max_contacts_per_world=64))
-    default = physx
+    default = newton_mjwarp
 
 
 
